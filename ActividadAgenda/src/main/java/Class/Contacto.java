f24360379f6c511ad9f6dc68e489311fa1fa4e98
Omar-Diaz09 <Al03065610@tecmilenio.mx>
2025-09-30 22:03:36 -0600

    Add files via upload

---
+++ ActividadAgenda/src/main/java/Class/Contacto.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Contacto {
+   int Id;
+    String Nombres;
+    String Apellidos;
+    String Direccion;
+    
+    public Contacto(int Id, String Nombre, String Apellidos, String Direccion){
+      this.Id = Id;
+      this.Nombres = Nombre;
+      this.Apellidos= Apellidos;
+      this.Direccion= Direccion;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getNombres() {
+        return Nombres;
+    }
+
+    public void setNombres(String Nombres) {
+        this.Nombres = Nombres;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String Apellidos) {
+        this.Apellidos = Apellidos;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    } 
+}
---
+++ ActividadAgenda/src/main/java/com/mycompany/actividadagenda/ActividadAgenda.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividadagenda;
+
+import Formulario.Inicio;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ActividadAgenda {
+
+    public static void main(String[] args) {
+       Inicio i = new Inicio();
+        i.setVisible(true);
+    }
+}
